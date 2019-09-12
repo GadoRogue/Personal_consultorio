360d3f478a7b23e115c8768b04d77d9df537e199
Gadiel <gadi77altair.gabg@gmail.com>
2019-09-12 02:53:02 -0400

    modificacion del main, cambio de nombre de la clase

--- demo/src/main/java/bo/edu/ucb/software1/demo/Repositorio_personal.java
+++ demo/src/main/java/bo/edu/ucb/software1/demo/Repositorio_personal.java
@@ -4,5 +4,4 @@ import org.hibernate.metamodel.model.convert.spi.JpaAttributeConverter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface Repositorio_personal extends JpaRepository<personal, Long> {
-
 }
